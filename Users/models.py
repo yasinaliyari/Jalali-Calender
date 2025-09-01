4230be56f9b1cdc7b3a0ba9c8cfcbd7463678e4e
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-01 19:31:09 +0330

    complete CustomeUser fields

--- Users/models.py
+++ Users/models.py
@@ -3,4 +3,17 @@ from django_jalali.db import models as jmodels
 
 
 class CustomUser(models.Model):
-    pass
+    M = 1
+    F = 2
+
+    GENDER_CHOICES = (
+        (M, "male"),
+        (F, "female"),
+    )
+    username = models.CharField(max_length=256, name="username")
+    full_name = models.CharField(max_length=256, name="full_name")
+    gender = models.CharField(choices=GENDER_CHOICES, name="gender")
+    national_code = models.CharField(max_length=10)
+    birthday_date = jmodels.jDateField(name="birthday_date")
+    ceremony_datetime = jmodels.jDateTimeField(name="ceremony_datetime")
+    country = models.CharField(default="Iran", name="country")
