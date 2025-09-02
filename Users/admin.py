e01619a16f45e5e4710711c6b3a53ceecea73f79
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-02 14:47:05 +0330

    register CustomUser model

--- Users/admin.py
+++ Users/admin.py
@@ -1,9 +1,29 @@
-from Users.models import Birthday
 from django.contrib import admin
+from django.contrib.admin import register
+from Users.models import CustomUser
+from django_jalali.admin.filters import JDateFieldListFilter
+import django_jalali.admin as jadmin
 
-#from django_jalali.admin.filters import JDateFieldListFilter
-#import django_jalali.admin as jadmin
 
+@register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = (
+        "username",
+        "get_first_name",
+        "get_last_name",
+        "gender",
+        "national_code",
+        "birthday_date",
+    )
+    search_fields = ("username", "full_name")
+    ordering = ("ceremony_datetime",)
 
-admin.site.register(Birthday)
+    def get_first_name(self, obj):
+        return obj.get_first_and_last_name()["first_name"]
 
+    get_first_name.short_description = "First name"
+
+    def get_last_name(self, obj):
+        return obj.get_first_and_last_name()["last_name"]
+
+    get_last_name.short_description = "Last name"
