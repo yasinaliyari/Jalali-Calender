1c2d3ad6f408bfac845263acc53231d95b52015c
yasin aliyari <yasinaliyari30@gmail.com>
2025-09-02 12:21:25 +0330

    create clean_national_code function in forms

--- Users/forms.py
+++ Users/forms.py
@@ -1,5 +1,25 @@
 from django import forms
+from django.core.exceptions import ValidationError
+from Users.models import CustomUser
 
 
 class CustomUserForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = CustomUser
+        fields = [
+            "username",
+            "full_name",
+            "gender",
+            "national_code",
+            "birthday_date",
+            "ceremony_datetime",
+            "country",
+        ]
+
+    def clean_national_code(self):
+        code = self.cleaned_data.get("national_code")
+        if len(code) != 10 or not code.isdigit():
+            raise ValidationError(
+                "The national code must be 10 digits and only contain numbers."
+            )
+        return code
